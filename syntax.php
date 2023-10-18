c610d363b9393bbfe573f5a2c4a84792a8625cab
Robert Spencer <admin@3rock.co.za>
2023-10-18 07:13:57 +0000

    Update PHP portion of plugin

--- syntax.php
+++ syntax.php
@@ -1,91 +1,61 @@
 <?php
+
+use dokuwiki\Extension\SyntaxPlugin;
+use dokuwiki\Extension\PluginInterface;
+
 /**
- * @file       clock/syntax.php
- * @brief      Show a clock in wikipage
+ * Clock Plugin: Show a clock in wikipages
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Luis Machuca Bezzaza <luis.machuca [at] gulix.cl>
- * @version    1.5
- * @date       2010-10-15
- * @link       http://www.dokuwiki.org/plugin:clock
- *
- *  This plugin's purpose is to display a clock using both 
- *  CSS and JavaScript techniques normally available.
- *  For a live test point a decent web browser to my wiki.
- *  http://ryan.gulix.cl/dw/desarrollo/
- *
- *  Greetings.
- *        - Luis Machuca Bezzaza a.k.a. 'ryan.chappelle'
  */
 
-if(!defined('DW_LF')) define('DW_LF',"\n");
- 
-if(!defined('DOKU_INC')) 
-define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
-if(!defined('DOKU_PLUGIN')) 
-define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'syntax.php');
- 
-/**
- * All DokuWiki plugins to extend the parser/rendering mechanism
- * need to inherit from this class
- */
-class syntax_plugin_clock extends DokuWiki_Syntax_Plugin {
- 
-    /**
-     * return some info
-     */
-    function getInfo() {
-        return array(
-            'author' => 'Luis Machuca Bezzaza',
-            'email'  => 'luis.machuca [at] gulix.cl',
-            'date'   => '2010-10-15',
-            'name'   => 'Clock Plugin',
-            'desc'   => $this->getLang('descr'),
-            'url'    => 'http://www.dokuwiki.org/plugin:clock',
-        );
-    }
- 
+class syntax_plugin_clock extends SyntaxPlugin
+{
     /**
      * What kind of syntax are we?
      */
-    function getType(){
+    public function getType()
+    {
         return 'substition';
     }
- 
-    /**
-     * What can we Do?
-     */
-    function getAllowedTypes() { 
-        return array(); 
-    }
- 
+
     /**
      * Where to sort in?
      */
-    function getSort(){
+    public function getSort()
+    {
         return 290;
     }
- 
+
     /**
      * What's our code layout?
-     */    
-    function getPType(){ 
-        return 'block'; 
+     */
+    public function getPType()
+    {
+        return 'block';
     }
- 
+
     /**
      * How do we get to the lexer?
      */
-    function connectTo($mode) {
-         $this->Lexer->addSpecialPattern (
-         '^\{\{clock\}\}$', $mode, 'plugin_clock');
- 
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('^\{\{clock\}\}$', $mode, 'plugin_clock');
+
     }
- 
+
     /**
      * Handle the match
+     *
+     * @param   string $match         The text matched by the patterns
+     * @param   int $state            The lexer state for the match
+     * @param   int $pos              The character position of the matched text
+     * @param   Doku_Handler $handler The Doku_Handler object
+     * @return  array                 Return an array with all data you want to use in render
      */
-    function handle($match, $state, $pos, &$handler){
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
         $data= array();
         $theCS= $this->getConf('clock_style');
         $theJS= $this->getConf('nojs_fallback');
@@ -95,44 +65,48 @@ class syntax_plugin_clock extends DokuWiki_Syntax_Plugin {
         /* compose the data array */
         $data['style']   = $theCS;
         $data['text']    = $theJS ? date('H:i:s') : $theCS;
-        
+
         /* Are we ready yet? */
         return $data;
-    }  
- 
+    }
+
     /**
      * Create output
+     *
+     * @param   string $format          String output format being rendered
+     * @param   Doku_Renderer $renderer The current renderer object
+     * @param   array $data             Data created by handler()
+     * @return  boolean                 Rendered correctly?
      */
-    function render($mode, &$renderer, $data) {
-      static $wasrendered= false;
+    public function render($format, Doku_Renderer $renderer, $data)
+    {
+        static $wasrendered= false;
 
-      if ($mode == 'xhtml') {
-        if (!$wasrendered) {
-          $nl   = DW_LF;
-          $cid  = $this->_get_clock_object_ID();
-          $html = $this->_clock_createblock_html($data);
-          $hbar = ($this->getConf('helpbar')) ? $this->_get_clock_helpbar() : '';
-          $renderer->doc .= <<<EOF
-<div id="$cid">$nl    $html $hbar$nl    </div><!-- end clock-->
+        if ($format == 'xhtml') {
+          if (!$wasrendered) {
+            $cid  = $this->_get_clock_object_ID();
+            $html = $this->_clock_createblock_html($data);
+            $hbar = ($this->getConf('helpbar')) ? $this->_get_clock_helpbar() : '';
+            $renderer->doc .= <<<EOF
+<div id="$cid">
+    $html $hbar
+</div><!-- end clock-->
 EOF;
-          $wasrendered= true;
-          }
-        else {
-          $renderer->doc.= <<<EOF
+            $wasrendered= true;
+          } else {
+            $renderer->doc .= <<<EOF
 <p><a href="#$cid" class="wikilink" title="Go To Clock">&#x231a; clock</a></p>
 EOF;
           }
-        return true;
-        }
-      else if ($mode == 'odt') {
-        return false; // may be implemented in the future
-        }
-      else if ($mode == 'text') {
-        if ($wasrendered) return true;
-        $text= ' ['. $this->_get_clock_object_ID(). ' '. date('H:i'). '] ';
-        $renderer->doc .= $text;
-        $wasrendered= true;
-        return true;
+          return true;
+        } elseif ($format == 'odt') {
+          return false; // may be implemented in the future
+        } elseif ($format == 'text') {
+          if ($wasrendered) return true;
+          $text = ' [' . $this->_get_clock_object_ID() . ' ' . date('H:i') . '] ';
+          $renderer->doc .= $text;
+          $wasrendered= true;
+          return true;
         }
       /* That's all about rendering that needs to be done, at the moment */
       return false;
@@ -142,49 +116,52 @@ EOF;
      * From this point onwards, local (helper) functions are implemented
      */
 
-    /** 
-     * @fn          dw_clock_object_ID
-     * @brief       returns ID for the clock object
-     *  This function sets the name used for the ID of the clock object.
-     *  If you change it, you must also update the #id tags in 'style.css'.
+    /**
+     * Returns ID for the clock object
+     *
+     * This function sets the name used for the ID of the clock object.
+     * If you change it, you must also update the #id tags in 'style.css'.
      */
-    function _get_clock_object_ID () {
-      return $this->getConf('clock_id');
-      }
+    private function _get_clock_object_ID()
+    {
+        return $this->getConf('clock_id');
+    }
 
     /**
-     * @fn          dw_clock_helpbar
-     * @brief       Returns the contents of the help bar
+     * Returns the contents of the help bar
+     *
      * The helpbar is displayed only when $conf['helpbar'] is set.
      */
-    private function _get_clock_helpbar () {
-      $p.= '<p class="helpbar" >';
-      $link= $this->getConf('clock_infopage');
-      if (empty($link) ) { // point to plugin page by default
-        $info= '[[doku>plugin:clock|Clock Plugin]]';
+    private function _get_clock_helpbar()
+    {
+        $p .= '<p class="helpbar" >';
+        $link = $this->getConf('clock_infopage');
+        if (empty($link)) { // point to plugin page by default
+          $info= '[[doku>plugin:clock|Clock Plugin]]';
+        } else {
+          $info= "[[$link|Info]]";
         }
-      else {
-        $info= "[[$link|Info]]";
-        }
-      $info= p_render('xhtml', p_get_instructions($info), $info);
-      $info= trim(substr($info, 4, -5)); // remove <p>, </p>
-      $p.= $info;
-      $p.= '</p>';       
-      return $p;
-      }
+        $info= p_render('xhtml', p_get_instructions($info), $info);
+        $info= trim(substr($info, 4, -5)); // remove <p>, </p>
+        $p .= $info;
+        $p .= '</p>';
+        return $p;
+    }
 
     /**
-     * @fn          _clock_createblock_html
-     * @brief       Creates the HTML code for the clock object.
+     * Creates the HTML code for the clock object
+     *
+     * @param  array $data  Data created by handler()
      */
-    private function _clock_createblock_html($data) {
-        $theStyle   = $data['style'];
-        $theText    = $data['text'];
-        $theDoLink  = $data['dolink'];
-        $theTarget  = $data['target'];
-        $codetext.= '<div id="clock_face" class="'. $theStyle. ' face">'. 
-                    $theText. '</div>';
+    private function _clock_createblock_html($data)
+    {
+        $theStyle  = $data['style'];
+        $theText   = $data['text'];
+        $theDoLink = $data['dolink'];
+        $theTarget = $data['target'];
+        $codetext .= '<div id="clock_face" class="'. $theStyle . ' face">' . $theText . '</div>';
         return $codetext;
-     }
+    }
 
 }
+
